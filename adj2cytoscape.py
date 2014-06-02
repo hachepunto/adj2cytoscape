75a3dd6474aa938d1e42ac3356ca015df6ee8512
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-06-02 14:33:30 -0500

    smaller indent, proper strip(), for instead of while

--- adj2cytoscape.py
+++ adj2cytoscape.py
@@ -5,15 +5,10 @@ parser.add_argument('adj', type=argparse.FileType('r'), nargs="+", help="One or 
 args    = parser.parse_args()
 
 for f in args.adj:
-       lineas = f.readlines()       
-       for l in lineas:
-              if not l.startswith('>'):
-                     columnas = l.split('\t')
-                     j = 0
-                     while j < len(columnas)-1:
-                            source     = columnas[0]
-                            gen_target = columnas[j+1]
-                            mi         = columnas[j+2]
-                            print "%s %s %s" % (source, mi, gen_target)
-                            j+=2
+    for l in f.readlines():
+        if not l.startswith('>'):
+            cols   = iter(l.strip().split('\t'))
+            source = cols.next()
+            for target, mi in ((item, cols.next()) for item in cols):
+                print "%s %s %s" % (source, mi, target)
 
